f200abf98de657da2e5b83efd44594f44b9b0130
Andy <aasecchi@gmail.com>
2023-01-06 10:22:30 -0300

    Se creo el boton para agregar lecturas

--- ConsumoLuz.py
+++ ConsumoLuz.py
@@ -29,20 +29,28 @@ class VentanaPrincipal(tk.Tk):
             validate="key",
             validatecommand=(self.register(self.validar_numeros),"%P")
         )
-        self.caja_lectura.place(x=150,y=45,width=142 ,height=20)
+        self.caja_lectura.place(x=80,y=45,width=142 ,height=20)
     
         self.etiqueta_fecha=ttk.Label(
             self,
             text="Fecha (aaaa-mm-dd):"
         )
-        self.etiqueta_fecha.place(x=20,y=70)
+        self.etiqueta_fecha.place(x=230,y=45)
 
         self.caja_fecha=ttk.Entry(
             self,
             validate="key",
             validatecommand=(self.register(self.validar_fecha),"%P")
         )
-        self.caja_fecha.place(x=150,y=70,width=142 ,height=20)    
+        self.caja_fecha.place(x=355,y=45,width=142 ,height=20)    
+
+        self.boton_agregar_lectura=tk.Button(
+            self,
+            text="Agregar",
+            command=self.agregar_lectura
+        )
+        self.boton_agregar_lectura.place(x=550,y=45,width=70, height=20)
+
 
     def validar_numeros(self, texto):
             if texto=="":
@@ -64,7 +72,30 @@ class VentanaPrincipal(tk.Tk):
             else:
                 chequeo.append(letra.isdecimal())
         return all(chequeo)
-  
+
+    def verificar_vacio(self, dato):
+        dato_verificar=dato
+        if dato_verificar=="":
+            messagebox.showwarning(
+                title="Advertencia",
+                message="Por favor es necesario ingresar un valor."
+            )
+            return False
+        else:
+            return dato_verificar
+
+
+    def agregar_lectura(self):
+        fecha=self.verificar_vacio(self.caja_fecha.get())
+        if fecha==True:
+            lectura=self.verificar_vacio(self.caja_lectura.get())
+            if lectura==True:
+                conn=sqlite3.connect(f'registo_luz.db')
+                cursor=conn.cursor()
+                cursor.execute("INSERT INTO consumoLuz VALUES (?, ?)", (fecha, lectura))
+                conn.commit()
+                conn.close()                
+
     def crear_base_datos(self):
         conn=sqlite3.connect(f'registo_luz.db')
         cursor=conn.cursor()
@@ -74,6 +105,7 @@ class VentanaPrincipal(tk.Tk):
         except sqlite3.OperationalError:
             # silenciar la excepción
             pass
+        conn.close()
 
 ventana_principal=VentanaPrincipal()
 ventana_principal.mainloop()
